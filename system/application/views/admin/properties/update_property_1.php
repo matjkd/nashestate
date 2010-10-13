cbfcab220ccfe56f161e18e5f3a6017d4c289a1e
mat sadler <mat@redstudio.co.uk>
2010-10-13 13:34:18 +0100

    got rid of tables on first property update form

--- system/application/views/admin/properties/update_property_1.php
+++ system/application/views/admin/properties/update_property_1.php
@@ -9,184 +9,140 @@ $attributes = array('class' => 'form', 'id' => 'form');
 echo "<label>Company Name:</label>".$row->company_name."<br/><br/>";
 ?>
 
-      <div style="clear:both;"></div>
-
-
-<div id="property_forms">
-
-
+<div style="clear:both;"></div>
 <div id='property_info'>
 
 
 
-        <label for="sale_rent">For Sale/Rent*</label>
+       <div class="admin_title">For Sale/Rent*</div>
        	<select id="sale_rent" name="sale_rent" DISABLED/>
 		<option <?php if($row->sale_rent==1){echo "selected";} ?>value="1">For Sale</option>
 		<option <?php if($row->sale_rent==2){echo "selected";} ?> value="2">For Rent</option>
 		</select>
 
 
- <table>
- 	<tr>
-	        <td class='leftcolumn'>
-	        	Title
-	        </td>
-			<td>
-				<div class="editdetails" id="property_title" style="width:150px; float:left;"><?php echo $row->property_title; ?></div>
-			</td>
-	</tr>
-	<tr>
-	        <td class='leftcolumn'>
-	        	Individual:
-	        </td>
-			<td>
-				<div class="editmembers" id="user_id" style="width:150px; float:left;"><?php echo $row->user_id; ?></div>
-			</td>
-	</tr>
-	<tr>
-	        <td class='leftcolumn'>
-	        	Date of Instruction:
-	        </td>
-			<td>
-				<div class="editdate" id="date_of_instruction" style="width:150px; float:left;"><?php echo $row->date_of_instruction; ?></div>
-			</td>
-	</tr>
-	
-	
- 	<tr>
-	        <td class='leftcolumn'>
-	        	Property Type
-	        </td>
-			<td>
-				<div class="edittype" id="property_type" style="width:150px; float:left;"><?php echo $row->property_type_name; ?></div>
-			</td>
-	</tr>
-	
-	<tr>
-	        <td class='leftcolumn'>
-	        	Address 1
-	        </td>
-			<td>
-				<div class="editdetails" id="property_address1" style="width:150px; float:left;"><?php echo $row->property_address1; ?></div>
-			</td>
-	</tr>
-	
-	<tr>
-	        <td class='leftcolumn'>
-	        	Address 2
-	        </td>
-			<td>
-				<div class="editdetails" id="property_address2" style="width:150px; float:left;"><?php echo $row->property_address2; ?></div>
-			</td>
-	</tr>
-	
-	<tr>
-	        <td class='leftcolumn'>
-	        	Address 3
-	        </td>
-			<td>
-				<div class="editdetails" id="property_address3" style="width:150px; float:left;"><?php echo $row->property_address3; ?></div>
-			</td>
-	</tr>
-	
-	<tr>
-	        <td class='leftcolumn'>
-	        	Address 4
-	        </td>
-			<td>
-				<div class="editdetails" id="property_address4" style="width:150px; float:left;"><?php echo $row->property_address4; ?></div>
-			</td>
-	</tr>
-	
-	<tr>
-	        <td class='leftcolumn'>
-	        	Postcode
-	        </td>
-			<td>
-				<div class="editdetails" id="property_address5" style="width:150px; float:left;"><?php echo $row->property_address5; ?></div>
-			</td>
-	</tr>
-</table> 
+
 </div>  
-<div id='property_info'>
-Property Info
-<table>
+<div style="clear:both;"></div>
+
+<div class="admin_title">Title</div>
+	<div class="admin_field" style="width:600px">
+		<div class="editdetails" id="property_title" style="width:600px"  ><?php echo $row->property_title; ?></div>
+	</div>
+<div style="clear:both;"></div>
+
+
+<div class="admin_left">
+
+	
+	
+	<div class="admin_title">Individual:</div>
+	<div class="admin_field">
+		<div class="editmembers" id="user_id" ><?php echo $row->user_id; ?></div>
+	</div>
+	
+	<div class="admin_title">Date of Instruction:</div>
+	<div class="admin_field">
+		<div class="editdate" id="date_of_instruction"><?php echo $row->date_of_instruction; ?></div>
+	</div>
+
+	<div class="admin_title">Property Type:</div>
+	<div class="admin_field">
+		<div class="edittype" id="property_type" ><?php echo $row->property_type_name; ?></div>
+	</div>
+	
+	<div class="admin_title">	Address 1:</div>
+	<div class="admin_field">
+		<div class="editdetails" id="property_address1"><?php echo $row->property_address1; ?></div>
+	</div>
+
+	<div class="admin_title">	Address 2:</div>
+	<div class="admin_field">
+		<div class="editdetails" id="property_address1"><?php echo $row->property_address2; ?></div>
+	</div>
+	
+	<div class="admin_title">	Address 3:</div>
+	<div class="admin_field">
+		<div class="editdetails" id="property_address1"><?php echo $row->property_address3; ?></div>
+	</div>
+	
+	<div class="admin_title">	Address 4:</div>
+	<div class="admin_field">
+		<div class="editdetails" id="property_address1"><?php echo $row->property_address4; ?></div>
+	</div>
+	
+	<div class="admin_title">	Postcode:</div>
+	<div class="admin_field">
+		<div class="editdetails" id="property_address1"><?php echo $row->property_address5; ?></div>
+	</div>
+
+
+</div>
+
+<div class="admin_right">
+
+		
 	<?php if($row->sale_rent==1){?>
-	<tr>
-	        <td class='leftcolumn'>
-	        	Sale Price (&euro;)
-	        </td>
-			<td>
-				<div class="editdetails" id="sale_price" style="width:150px; float:left;"><?php echo $row->sale_price; ?></div>
-			</td>
-	</tr>
-	<tr>
-	        <td class='leftcolumn'>
-	        	Sale Payment
-	        </td>
-			<td>
-				<div class="editpayment" id="sale_payment" style="width:150px; float:left;"><?php echo $row->sale_payment; ?></div>
-			</td>
-	</tr>
+	<div class="admin_title">Sale Price (&euro;)</div>
+	<div class="admin_field">
+		<div class="editdetails" id="sale_price" ><?php echo $row->sale_price; ?></div>
+	</div>
+	<div class="admin_title">Sale Payment</div>
+	<div class="admin_field">
+		<div class="editpayment" id="sale_payment" ><?php echo $row->sale_payment; ?></div>
+	</div>
 	<?php } ?>
 	
 	<?php if($row->sale_rent==2){?>
-	<tr>
-	        <td class='leftcolumn'>
-	        	Rent Price (&euro;)
-	        </td>
-			<td>
+	<div class="admin_title">Rent Price (&euro;)</div>
+	<div class="admin_field">
+		<div class="editdetails" id="rent_price"><?php echo $row->rent_price; ?></div>
+	</div>
 				<div class="editdetails" id="rent_price" style="width:150px; float:left;"><?php echo $row->rent_price; ?></div>
-			</td>
-	</tr>
-	<tr>
-	        <td class='leftcolumn'>
-	        	Rent Period
-	        </td>
-			<td>
-				<div class="editperiod" id="rent_period" style="width:150px; float:left;"><?php echo $row->rent_period; ?></div>
-			</td>
-	</tr>
+	
+	
+	<div class="admin_title">Rent Period</div>
+	<div class="admin_field">
+		<div class="editperiod" id="rent_period" ><?php echo $row->rent_period; ?></div>
+	</div>
+				
+		
 	<?php } ?>
-	<tr>
-	        <td class='leftcolumn'>
-	        	Available From:
-	        </td>
-			<td>
-				<div class="editdate" id="available_from" style="width:150px; float:left;"><?php echo $row->available_from; ?></div>
-			</td>
-	</tr>
 	
-	<tr>
-	        <td class='leftcolumn'>
-	        	Build Size (m2)
-	        </td>
-			<td>
-				<div class="editdetails" id="build_size" style="width:150px; float:left;"><?php echo $row->build_size; ?></div>
-			</td>
-	</tr>
+	<div class="admin_title">Available From</div>
+	<div class="admin_field">
+		<div class="editdate" id="available_from" ><?php echo $row->available_from; ?></div>
+	</div>
 	
-	<tr>
-	        <td class='leftcolumn'>
-	        	Plot Size (m2)
-	        </td>
-			<td>
-				<div class="editdetails" id="plot_size" style="width:150px; float:left;"><?php echo $row->plot_size; ?></div>
-			</td>
-	</tr>
+	<div class="admin_title">Build Size (m2)</div>
+	<div class="admin_field">
+			<div class="editdetails" id="build_size"><?php echo $row->build_size; ?></div>
+	</div>
 	
-	<tr>
-	        <td class='leftcolumn'>
-	        	General Area
-	        </td>
-			<td>
-				<div class="editarea" id="general_area" style="width:150px; float:left;"><?php echo $row->area; ?></div>
-			</td>
-	</tr>
-</table>
-</div> 
+	
+	<div class="admin_title">Plot Size (m2)</div>
+	<div class="admin_field">
+			<div class="editdetails" id="plot_size" ><?php echo $row->plot_size; ?></div>
+	</div>
+	
+	<div class="admin_title">Number of Floors</div>
+	<div class="admin_field">
+		<div class="editdetails" id="floors" ><?php echo $row->floors; ?></div>
+	</div>
+	
+	<div class="admin_title">General Area</div>
+	<div class="admin_field">
+		<div  class="editarea" id="general_area"><?php echo $row->area; ?></div>
+	</div>
+
+	
+</div>
+  <div style="clear:both;"></div>
+
+
+
+
 <div style="clear:both;"></div>
 <?php 
 endforeach;
 ?>
-</div>
