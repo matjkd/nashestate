3218003aaa097c1d65d56a61c549f34dd09c2673
mat sadler <mat@redstudio.co.uk>
2011-01-24 10:08:32 +0000

    changed property of the week

--- system/application/views/sidebar/property_of_week.php
+++ system/application/views/sidebar/property_of_week.php
@@ -3,7 +3,9 @@
 <?php 
 if($featured_property != NULL) { ?>
 <div id="property_box">
+
 <?php foreach($featured_property as $row):?>
+
 <a href="<?=base_url()?>property/display/<?=$row['property_ref_no']?>">
 <img width="310px" src="<?=base_url()?>images/properties/<?=$row['property_ref_no']?>/medium/<?=$row['filename']?>">
 <div id="property_box_text"><strong>Property of the Week</strong></div>
