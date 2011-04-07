edcdf210e20b7af34ffa4f2ab838fc2cda1ee4e7
mat sadler <mat@redstudio.co.uk>
2011-04-07 14:56:40 +0100

    added link to full size image

--- system/application/views/property/gallery.php
+++ system/application/views/property/gallery.php
@@ -9,10 +9,15 @@
     <ul class="thumbs noscript">
     	<?php foreach($property_images as $image):?>
         <li>
-            <a class="thumb" name="optionalCustomIdentifier" href="<?=base_url()?>images/properties/<?=$image->property_id?>/<?=$image->filename?>" title="<?=$image->filename?>">
+            <a class="thumb" name="optionalCustomIdentifier" href="<?=base_url()?>images/properties/<?=$image->property_id?>/medium/<?=$image->filename?>" title="<?=$image->filename?>">
                 <img  src="<?=base_url()?>/images/properties/<?=$image->property_id?>/thumbs/<?=$image->filename?>" alt="<?=$image->filename?>" />
             </a>
-           <?php if(isset($image->image_description)) { echo "<div class='caption'>".$image->image_description."</div>"; }?>
+            <div class='caption'>
+            <a  href="<?=base_url()?>images/properties/<?=$image->property_id?>/<?=$image->filename?>" title="<?=$image->filename?>">View Full Size Image</a><br/>
+           <?php if(isset($image->image_description)) { echo $image->image_description; }?>
+           </div>
+           
+           
         </li>
         <?php endforeach;?>
        
