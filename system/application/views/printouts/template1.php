ca798afe1bf7062d2e6215f94402f8c642a01743
mat sadler <mat@redstudio.co.uk>
2011-04-18 17:01:36 +0100

    added test file for pdf

--- system/application/views/printouts/template1.php
+++ system/application/views/printouts/template1.php
@@ -1,8 +1,6 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
-
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" /> 
 <style>
 body {
   margin: 0pt 22pt 24pt 22pt;
