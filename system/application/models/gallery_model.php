39fa6c6e93bd4bc989a21403fedba4df78cb522a
mat sadler <github@redstudio.co.uk>
2017-07-20 12:20:32 +0100

    just resize the large

--- system/application/models/gallery_model.php
+++ system/application/models/gallery_model.php
@@ -164,12 +164,8 @@ class Gallery_model extends Model {
         
          //watermark the medium  
         $config0 = array(
-            'source_image' =>  $original_image,
-            'image_library' => 'GD2',
-            'wm_text' => 'Nash Homes 2017',
-            'wm_type' => 'overlay',
-            'wm_overlay_path' => $this->config_base_path . 'images/watermark.png',
-            'wm_vrt_alignment' => 'middle',
+            'source_image' =>  $this->gallery_path . '/' . $property_id . '/large/'. $filename,
+           
             'new_image' => $this->gallery_path . '/' . $property_id . '/medium/',
             'maintain_ratio' => true,
             'width' => 400,
@@ -178,7 +174,7 @@ class Gallery_model extends Model {
         );
 
         $this->load->library('image_lib', $config0);
-        $this->image_lib->watermark();
+       
         $this->image_lib->clear();    
         
     }
