5564f057c65a907af2f5b4fd875a3260b2a3c337
mat sadler <github@redstudio.co.uk>
2017-07-28 14:03:22 +0100

    yeah

--- system/application/views/admin/properties/slideshow_form.php
+++ system/application/views/admin/properties/slideshow_form.php
@@ -52,12 +52,7 @@ $.post("<?=base_url()?>admin/images/ajaxsort", { pageorder: order1 } );
 				<a href="#">
 					<img height="100px" width="135px" src="<?=base_url()?>images/properties/<?=$image->property_id?>/thumbs/<?=$image->filename?>" />
 				</a>
-				<div style="float:left;">On Printout:</div> <div  style="float:left;" class="printable" id="<?=$image->image_id?>" style="width:150px; float:left;">
-				<?php 
-				if($image->printable==0) {echo "No";}; 
-				if($image->printable==1) {echo "Yes";};
-				?>
-				</div>
+				
 				
 				<div style="clear:both; float:left;">Order:</div> 	<div  style="float:left;" class="image_order" id="<?=$image->image_id?>" style="width:150px; float:left;"><?=$image->print_order?></div>
 				<div style="clear:both; float:left; padding-top:10px;">
