727d96b3821ca5e407c9c6df59087f6167f8e7af
mat sadler <github@redstudio.co.uk>
2018-02-15 14:08:38 +0000

    hmmm

--- system/application/views/template/crystal/frontpage.php
+++ system/application/views/template/crystal/frontpage.php
@@ -1,15 +1,10 @@
 <section id="slider">
-			<div class="container">
-				<div class="row">
-					<div id="sidebar" class=" pull-left span4 searchbox">
-						<?=$this -> load -> view('template/crystal/searchbox') ?>
-					</div>
+		
 
 					
 
-					<div class="span8 searchbox">
+					<div class=" searchbox">
 						<?=$this -> load -> view('template/crystal/slideshowV2') ?>
 					</div>
-				</div>
-			</div>
-		</section>
+				
+</section>
--- system/application/views/template/crystal/map.php
+++ system/application/views/template/crystal/map.php
@@ -39,18 +39,16 @@
 <section id="map">
 			<div class="container">
 				<div class="row">
-					<div id="sidebar" class=" pull-left span4 searchbox">
-						<?=$this -> load -> view('template/crystal/searchbox') ?>
-					</div>
+					
 
 					
 
-					<div class="span8 searchbox">
+					
 						<div id="map_canvas" style="width: 100%; height: 303px;">
 <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=nash+homes+mallorca&amp;aq=&amp;sll=38.284226,-2.921077&amp;sspn=13.646525,20.148926&amp;t=m&amp;ie=UTF8&amp;hq=nash+homes+mallorca&amp;hnear=&amp;ll=39.535424,2.571756&amp;spn=0.002896,0.00456&amp;z=17&amp;iwloc=B&amp;output=embed"></iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=nash+homes+mallorca&amp;aq=&amp;sll=38.284226,-2.921077&amp;sspn=13.646525,20.148926&amp;t=m&amp;ie=UTF8&amp;hq=nash+homes+mallorca&amp;hnear=&amp;ll=39.535424,2.571756&amp;spn=0.002896,0.00456&amp;z=17&amp;iwloc=B" style="color:#0000FF;text-align:left">View Larger Map</a></small>
 
 					</div>
-				</div>
+				
 			</div>
     </div>
 		</section>
