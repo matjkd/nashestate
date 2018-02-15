bf51af8da070df1b075343acdaedbb76932285fc
mat sadler <github@redstudio.co.uk>
2018-02-15 15:25:45 +0000

    remove nearby sales

--- system/application/views/search/search_list.php
+++ system/application/views/search/search_list.php
@@ -93,55 +93,7 @@ if($property['sold_rented'] == 1) {?>
 	if ($property['rooms'] >= $beds && $maxbeds >= $property['rooms']) {
 		?>
 
-	<div  class="result row">
-
-		<div class="span8 search_list  " id="search_content">
-			<div class="row">
-			<div class="span6">
-				<p class="lead"><?= $property['property_title'] ?> </p> 
-				<?= $property['property_type_name'] ?>
-					:: <?= $property['area'] ?> <br /> Bedrooms:
-				<?= $property['rooms'] ?>
-				<br />
-				<p>
-					<?php
-					if ($property['alt_description'] == NULL) {
-						$description = $property['description'];
-					} else {
-						$description = $property['alt_description'];
-					}
-
-
-					$description = strip_tags($description);
-					$description = substr($description, 0, 130);
-					echo "" . $description . "...";
-					?><br/>
-					<a   href="<?= base_url() ?>property/display/<?= $property['property_ref_no'] ?>">
-						<span class="btn btn btn-welcome">Read More</span>
-						</a> <br />
-				</p>
-
-
-
-
-				<strong>Ref: &#35;<?= $property['property_ref_no'] ?> &nbsp; &nbsp;
-					&nbsp; &nbsp; &nbsp; Price: <?= number_format($property['sale_price']) ?>
-					&euro;
-				</strong>
-			</div>
-
-				<div class="span2">
-				<?php if (isset($property['filename'])) { ?>
-				<img width="100%" 
-					src="<?= base_url() ?>images/properties/<?= $property['property_ref_no'] ?>/<?= $property['filename'] ?>">
-				<?php } ?>
-			</div>
-			</div>
-		</div>
-
-
-
-	</div>
+	
 
 	<?php
 	} else {
