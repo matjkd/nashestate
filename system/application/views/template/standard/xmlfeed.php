e2b8259e38b98378f38464c449f9a5f8d43510ca
mat sadler <github@redstudio.co.uk>
2021-04-22 11:06:00 +0100

    Update xmlfeed.php

--- system/application/views/template/standard/xmlfeed.php
+++ system/application/views/template/standard/xmlfeed.php
@@ -2,8 +2,10 @@
   <kyero>
     <feed_version>3</feed_version>
   </kyero>
+  <?php foreach ($properties as $property): ?>
 <property>
+  something
 </property>
-  
+  <?php endforeach; ?>
 
 </root>
