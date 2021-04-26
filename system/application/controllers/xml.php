1821cde24aba066cd570b2bcd47a2a94ce677622
mat sadler <github@redstudio.co.uk>
2021-04-26 23:33:48 +0100

    Update xml.php

--- system/application/controllers/xml.php
+++ system/application/controllers/xml.php
@@ -318,7 +318,7 @@ $this->load->view('template/standard/xmlfeed');
 //		}		
 				
 				
-		echo "<image id='"..$imagedata->image_id"'>";		
+		echo "<image id='".$imagedata->image_id."'>";		
 //		echo "<url>".$theImage;."</url>";
 		echo "</image>";
 				endforeach;
